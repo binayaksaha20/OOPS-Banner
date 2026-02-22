ac4c6b8cbcec72204b566f86292e4ddcfef86ee6
binayaksaha20 <sahabinayak0@gmail.com>
2026-02-22 22:09:00 +0530

    Coded UC2 Program

---
+++ OOPSBannerApp.java
@@ -0,0 +1,11 @@
+class OOPSPattern {
+    public static void main(String[] args) {
+        System.out.println(" *    *    **   ** ");
+        System.out.println("*   *  *   *   *       *   * ");
+        System.out.println("*   *  *   *   *       *   * ");
+        System.out.println("*   *  *   *   **   ** ");
+        System.out.println("*   *  *   *   *           * ");
+        System.out.println("*   *  *   *   *           * ");
+        System.out.println(" *    *    *       *** ");
+    }
+}
