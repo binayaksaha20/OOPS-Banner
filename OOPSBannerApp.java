2dc03cc021d1e5978d1314ab72586ff23eea3aaa
binayaksaha20 <bs8236@srmist.edu.in>
2026-02-23 18:32:46 +0530

    Replace banner pattern with new design

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,10 @@
 class OOPSPattern {
     public static void main(String[] args) {
-        System.out.println(" *    *    **   ** ");
-        System.out.println("*   *  *   *   *       *   * ");
-        System.out.println("*   *  *   *   *       *   * ");
-        System.out.println("*   *  *   *   **   ** ");
-        System.out.println("*   *  *   *   *           * ");
-        System.out.println("*   *  *   *   *           * ");
-        System.out.println(" *    *    *       *** ");
-    }
+    System.out.println("  *****  " + "  *****  " + "  *****  " + "  *****  ");
+    System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
+    System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
+    System.out.println(" *     * " + " *     * " + "  *****  " + "  *****  ");
+    System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+    System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+    System.out.println("  *****  " + "  *****  " + " *       " + "  *****  ");
 }
