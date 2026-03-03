0616a0707e7bc81a976a05588ac4dcb7bfa8eebe
binayaksaha20 <bs8236@srmist.edu.in>
2026-03-03 08:51:54 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,1 @@
-class OOPSPattern {
-    void main() {
-    System.out.println("  *****    *****    *****    *****  ");
-    System.out.println(" *     *  *     *  *     *  *       ");
-    System.out.println(" *     *  *     *  *     *  *       ");
-    System.out.println(" *     *  *     *   *****    *****  ");
-    System.out.println(" *     *  *     *  *              * ");
-    System.out.println(" *     *  *     *  *              * ");
-    System.out.println("  *****    *****   *         *****  ");
-}
-}
+
