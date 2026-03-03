f5764e9277baa3a28dfeb29da95c3a88b7d8f41f
binayaksaha20 <sahabinayak0@gmail.com>
2026-03-03 09:06:51 +0530

    UC4

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,18 @@
-class OOPSPattern {
-    void main() {
-    System.out.println("  *****    *****    *****    *****  ");
-    System.out.println(" *     *  *     *  *     *  *       ");
-    System.out.println(" *     *  *     *  *     *  *       ");
-    System.out.println(" *     *  *     *   *****    *****  ");
-    System.out.println(" *     *  *     *  *              * ");
-    System.out.println(" *     *  *     *  *              * ");
-    System.out.println("  *****    *****   *         *****  ");
-	}
+public class ArrayLoop {
+    public static void main(String[] args) {
+        // 1. Create String array with 7 lines
+        String[] lines = new String[7];
+        // 2. Populate array using String.join()
+        lines[0] = String.join(" ", " *** ", "  ***  ", " ***** ", " ***** ");
+        lines[1] = String.join(" ", "*   *", " *   * ", "*     *", "*     *");
+        lines[2] = String.join(" ", "*   *", " *   * ", "*     *", "*");
+        lines[3] = String.join(" ", "*   *", " *   * ", " ***** ", " ***** ");
+        lines[4] = String.join(" ", "*   *", " *   * ", "*       ", "      *");
+        lines[5] = String.join(" ", "*   *", " *   * ", "*       ", "*     *");
+        lines[6] = String.join(" ", " *** ", "  ***  ", "*       ", " ***** ");
+        // 3. Enhanced for-loop to print banner
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
 }
